54ef873e6a0f6284a6206a81b61c5820b96ff0ff
Jhonas Moura <jhonaspradomoura@gmail.com>
2021-04-29 23:41:47 -0300

    added type argparse on tuner

--- MPNet/AE/tune.py
+++ MPNet/AE/tune.py
@@ -47,10 +47,10 @@ def tuning(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--batch_size', default=100)
-    parser.add_argument('--num-epochs', default=25)
-    parser.add_argument('--num_gpus', default=1)
-    parser.add_argument('--num_cpus', default=1)
+    parser.add_argument('--batch_size', default=100, type=int)
+    parser.add_argument('--num-epochs', default=25, type=int)
+    parser.add_argument('--num_gpus', default=1, type=int)
+    parser.add_argument('--num_cpus', default=1, type=int)
     
     args = parser.parse_args()
     
--- MPNet/data_loader.py
+++ MPNet/data_loader.py
@@ -13,19 +13,26 @@ project_path = f"{os.path.abspath(__file__).split('mpnet')[0]}mpnet"
 
 
 def plot_sample(center_list, path_list):
-    fig = make_subplots(rows=2, cols=3)
     
+    fig = make_subplots(rows=2, cols=3)
     for n, (center, paths) in enumerate(zip(center_list, path_list)):
-        x = center[:, 0]
-        y = center[:, 1]
-        
+        obstacles = []
+        for point in center:
+            x, y = point
+            obstacles.extend([[x - 2.5, y - 2.5], [x - 2.5, y + 2.5], [x + 2.5, y + 2.5], [x + 2.5, y - 2.5],
+                              [x - 2.5, y - 2.5], [None, None]])
+        obstacles = np.array(obstacles)
+        x = obstacles[:, 0]
+        y = obstacles[:, 1]
         fig.add_trace(go.Scatter(x=x, y=y, fill="toself", fillcolor="black", name="obstacle"),
-                      row=(int(n / 3) + 1), col=n % 3)
+                      row=(int(n / 3) + 1), col=(n % 3) + 1)
+        
         for path in paths:
             x = path[:, 0]
             y = path[:, 1]
             
-            fig.add_trace(go.Scatter(x=x, y=y, mode='lines+markers'))
+            fig.add_trace(go.Scatter(x=x, y=y, mode='lines+markers'), row=(int(n / 3) + 1), col=(n % 3) + 1)
+            
     fig.show()
 
 
